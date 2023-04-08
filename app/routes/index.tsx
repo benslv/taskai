23a887c5b339edaf325e676192374d8f81656f7a
benslv <bensilverman10@gmail.com>
2023-04-08 12:34:55 +0100

    add server validation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,11 @@
+import type { ActionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
 import { Plus } from "iconoir-react";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useLocalStorage from "use-local-storage";
+import { z } from "zod";
+import { v4 as uuidv4 } from "uuid";
 
 type Task = {
 	id: number;
@@ -8,30 +13,59 @@ type Task = {
 	duration: number;
 };
 
+const taskSchema = z.object({
+	name: z
+		.string()
+		.min(1, { message: "You have to enter a task name" })
+		.max(50, { message: "Task name is too long" }),
+});
+
+export async function action({ request }: ActionArgs) {
+	const body = await request.formData();
+
+	console.log("in the action");
+
+	const result = taskSchema.safeParse({
+		name: body.get("task"),
+	});
+
+	if (!result.success) {
+		return {
+			status: 400,
+			body: result.error,
+		};
+	}
+
+	const task = result.data;
+
+	const randomDuration = Math.floor(Math.random() * 10) + 1;
+
+	// artificial delay
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+
+	return json({
+		id: uuidv4(),
+		title: task.name,
+		duration: randomDuration,
+	});
+}
+
 export default function Index() {
 	const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
-	const [inputValue, setInputValue] = useState("");
 
-	const addTask = () => {
-		if (inputValue.trim().length === 0) return;
-
-		const newTask = {
-			id: tasks.length + 1,
-			title: inputValue,
-			duration: 10,
-		};
-		setTasks([...tasks, newTask]);
-
-		setInputValue("");
-	};
+	const todoFetcher = useFetcher();
+	const isSubmitting = todoFetcher.state === "submitting";
+	const isLoading = todoFetcher.state === "idle" && todoFetcher.data;
 
 	const deleteTask = (id: number) => {
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInputValue(e.target.value);
-	};
+	useEffect(() => {
+		if (isLoading && todoFetcher.data.id !== tasks[tasks.length - 1]?.id) {
+			setTasks([...tasks, todoFetcher.data]);
+		}
+	}, [setTasks, tasks, isLoading, todoFetcher.data]);
 
 	return (
 		<div className="mx-auto flex h-full max-w-xl flex-col justify-center gap-y-4">
@@ -68,26 +102,51 @@ export default function Index() {
 									</div>
 								);
 							})}
+							{isSubmitting && (
+								<div className="zinc-800 flex items-center gap-x-2 rounded-md border border-zinc-600 bg-zinc-900 bg-gradient-to-r p-2 px-4 shadow">
+									<div role="status">
+										<svg
+											aria-hidden="true"
+											className="-ml-2 h-5 w-5 animate-spin fill-white text-zinc-700 dark:text-zinc-700"
+											viewBox="0 0 100 101"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg">
+											<path
+												d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+												fill="currentColor"
+											/>
+											<path
+												d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+												fill="currentFill"
+											/>
+										</svg>
+										<span className="sr-only">Loading...</span>
+									</div>
+									<p className="text-zinc-400">
+										{todoFetcher.submission.formData.get("task") as string}
+									</p>
+								</div>
+							)}
 						</div>
 					</>
 				) : (
 					<p className="text-zinc-400">Add some tasks :)</p>
 				)}
-				<div className="mt-4 flex items-center">
+				<todoFetcher.Form className="mt-4 flex items-center" method="post">
 					<input
 						type="text"
 						name="task"
-						value={inputValue}
-						onChange={handleInputChange}
+						min={1}
+						max={50}
 						placeholder="e.g. Reply to John's email"
 						className="w-full min-w-0 rounded-tl-md rounded-bl-md  border border-zinc-600 bg-zinc-800 px-4 py-2 shadow-lg transition-colors focus-visible:bg-zinc-900 focus-visible:outline-none"
 					/>
 					<button
-						onClick={addTask}
+						type="submit"
 						className="rounded-tr-md rounded-br-md border border-zinc-600 bg-zinc-600 p-2 shadow-lg transition-colors hover:border-zinc-500 hover:bg-zinc-500">
 						<Plus />
 					</button>
-				</div>
+				</todoFetcher.Form>
 			</div>
 			<div className="border-1 rounded-lg border border-zinc-700 bg-zinc-800 p-4 shadow">
 				<h2 className="text-xl text-zinc-200">Status</h2>
